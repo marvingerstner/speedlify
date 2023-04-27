935468d7c871a9057d2291a7b62eed440fb65623
Marvin Gerstner <info@marvin-gerstner.de>
2023-04-27 19:06:48 +0200

    Create marvin-gerstner.js

---
+++ _data/sites/marvin-gerstner.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "marvin-gerstner.de", // optional, falls back to object key
+	description: "The default sites that get tested",
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://www.marvin-gerstner.de/"
+	]
+};
